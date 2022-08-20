8a075f9c985e215a2aa0e831c8075526eb6c92d3
saruaralam-swd <saruaralam2001@gmail.com>
2022-08-20 16:33:28 +0600

    comment

--- js/case.js
+++ js/case.js
@@ -1,18 +1,14 @@
 //case-minus-btn case-input case-plus-btn case-price
 // Subtotal, tax, total
 
-// case: plus button 
+// case: + button 
 document.getElementById('case-plus-btn').addEventListener('click', () => {
   const casePrice = QuantityPrice(true, 'case-input', 'case-price', 5);
   console.log(casePrice);
-
-  
 });
 
-// case: minus button 
+// case: - button 
 document.getElementById('case-minus-btn').addEventListener('click', () => {
   const casePrice = QuantityPrice(false, 'case-input', 'case-price', 5);
   console.log(casePrice);
-
-  
 });
--- js/phone.js
+++ js/phone.js
@@ -1,14 +1,14 @@
 //phone-minus-btn, phone-input, phone-plus-btn, phone-price
 // subtotal, tax, total
 
-
-
+// phone: + button 
 document.getElementById('phone-plus-btn').addEventListener('click', () => {
   const phonePrice =  QuantityPrice(true, 'phone-input', 'phone-price', 10);
-
+  console.log(phonePrice);
 });
 
+// phone: - button 
 document.getElementById('phone-minus-btn').addEventListener('click', () => {
   const phonePrice =  QuantityPrice(false, 'phone-input', 'phone-price', 10);
-
+  console.log(phonePrice);
 });
